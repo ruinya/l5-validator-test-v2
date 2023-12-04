78c7ed3a5e75683ab337ad8221c37561aae5a58d
ruinya <mistershok11239@gmail.com>
2023-12-04 13:23:42 +0300

    pass 4/5 fix

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,24 +1,25 @@
+import _ from 'lodash';
+
 export default class ArraySchema {
-    constructor(validators) {
-      this.validators = [...validators];
-    }
-  
-    isValid(array) {
-      return this.validators.every((validator) => validator(array));
-    }
-  
-    maxDepth(max) {
-      const validator = (values) => {
-        const iter = (element, depth = -1) => {
-          if (!Array.isArray(element)) {
-            return depth;
-          }
-          const result = element.map((value) => iter(value, depth + 1));
-          return Math.max(...result);
-        };
-        return iter(values) <= max;
+  validators = [(value) => Array.isArray(value)];
+
+  isValid(array) {
+    return this.validators.every((validator) => validator(array));
+  }
+
+  maxDepth(max) {
+    const validator = (values) => {
+      const iter = (element, depth = -1) => {
+        if (!Array.isArray(element)) {
+          return depth;
+        }
+        const result = element.map((value) => iter(value, depth + 1));
+        return _.flattenDeep(result);
       };
-  
-      return new ArraySchema([...this.validators, validator]);
-    }
+      return iter(values).every((val) => val <= max);
+    };
+
+    this.validators.push(validator);
+    return this;
+  }
 }
